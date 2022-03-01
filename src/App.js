9c02c90fdc4199c097a10bb828e6455d80c2c8b6
Aleks2904 <Garmatin171@yandex.ru>
2022-03-01 09:57:22 +0700

    lesson 2

--- src/App.js
+++ src/App.js
@@ -1,34 +1,70 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const InputComponent = () =>{
-    const [message, setMessage] = React.useState('');
+const InputComponent = ({setList, list}) =>{
+    const [message, setMessage] = useState('');
 
-    function updateMessage(e){
-        setMessage(e.target.value)
+    function setListMessages(){
+        setList([...list, message])
+        setMessage('')
     }
+    function setMessageFn(e){
+        setMessage( e.target.value)
+    }
+
+    useEffect(()=>{
+        const text = 'hi, im bot';
+        const lastMessage = list[list.length - 1];
+        let time = null;
+
+        if(lastMessage !== text && list.length > 0){
+            time = setTimeout(()=>{
+                setList([...list, text])
+            },500)
+        }
+
+        return () =>{
+            clearInterval(time)
+        }
+    })
 
     return(
         <div>
-            <input onInput={updateMessage} type="text"/>
-            <MessageComponent value={message}/>
+            <input value={message} onInput={setMessageFn} type="text"/>
+            <button onClick={setListMessages}>add</button>
         </div>
     )
-}
+};
 
-const MessageComponent = ({value}) =>{
+const MessageComponent = ({list}) =>{
     return(
-        <p>
-            message: {value}
-        </p>
+        <div>
+            {list.map((el, index)=>(
+                <p key={index}>
+                    message: {el}
+                </p>
+            ))}
+        </div>
     )
-}
+};
+
+const MessageList = () => {
+    const [list, setList] = useState([]);
+
+    return(
+        <div>
+            <InputComponent setList={setList} list={list}/>
+            <br/>
+            <MessageComponent list={list} />
+        </div>
+    )
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <InputComponent />
+          <MessageList/>
       </header>
     </div>
   );
